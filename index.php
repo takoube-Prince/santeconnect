5b49825015a8efc6c867d684349311ae9b378f24
takoube-Prince <princearistide1@gmail.com>
2026-05-26 20:28:50 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,438 @@
+
+<?php
+include_once('hms/include/config.php');
+if(isset($_POST['submit']))
+{
+$name=$_POST['fullname'];
+$email=$_POST['emailid'];
+$mobileno=$_POST['mobileno'];
+$dscrption=$_POST['description'];
+$query=mysqli_query($con,"insert into tblcontactus(fullname,email,contactno,message) value('$name','$email','$mobileno','$dscrption')");
+echo "<script>alert('Your information succesfully submitted');</script>";
+echo "<script>window.location.href ='index.php'</script>";
+
+} ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title> Santé Connect - Homepage </title>
+
+    <link rel="shortcut icon" href="assets/images/fav.jpg">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/fontawsom-all.min.css">
+     <link rel="stylesheet" href="assets/css/animate.css">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+</head>
+
+    <body>
+
+    <!-- ################# Header Starts Here#######################--->
+    
+      <header id="menu-jk">
+    
+        <div id="nav-head" class="header-nav">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-2 col-md-3  col-sm-12" style="color:#000;font-weight:bold; font-size:50px; margin-top: 1% !important;"><a href="#">SC</a>
+                       <a data-toggle="collapse" data-target="#menu" href="#menu" ><i class="fas d-block d-md-none small-menu fa-bars"></i></a>
+                    </div>
+                    <div id="menu" class="col-lg-8 col-md-9 d-none d-md-block nav-item">
+                        <ul>
+                            <li><a href="#">Accueil</a></li>
+                            <li><a href="#services">Services</a></li>
+                            <li><a href="#about_us">À propos</a></li>
+                            <li><a href="#gallery">Galerie</a></li>
+                            <li><a href="#contact_us">Contactez-nous</a></li>
+                            <li><a href="#logins">Connexions</a></li>  
+                        </ul>
+                    </div>
+                    <div class="col-sm-2 d-none d-lg-block appoint">
+                        <a class="btn btn-success" href="hms/user-login.php">Prendre rendez-vous</a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </header>
+    
+     <!-- ################# Slider Starts Here#######################--->
+
+    <div class="slider-detail">
+
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            </ol>
+
+
+   
+
+
+            <div class="carousel-inner">
+                <div class="carousel-item ">
+                    <img class="d-block w-100" src="assets/images/slider/slider_5.jpg" alt="Second slide">
+                    <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">Santé Connect</h5>
+            
+                         
+                    
+                    </div>
+                </div>
+                
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/images/slider/slider_4.jpg" alt="Third slide">
+                      <div class="carousel-cover"></div>
+                    <div class="carousel-caption vdg-cur d-none d-md-block">
+                        <h5 class="animated bounceInDown">Santé Connect</h5>
+            
+                         
+                    
+                    </div>
+              
+                </div>
+                
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Précédent</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Suivant</span>
+            </a>
+        </div>
+
+
+    </div>
+    
+  <!--  ************************* Logins ************************** -->
+    
+    
+     <section id="logins" class="our-blog container-fluid">
+        <div class="container">
+        <div class="inner-title">
+
+                <h2>Connexions</h2>
+            </div>
+            <div class="col-sm-12 blog-cont">
+                <div class="row no-margin">
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                                <img src="assets/images/patient4.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Espace Patient</h6>
+                                <a href="hms/user-login.php" target="_blank">
+                                    <button class="btn btn-success btn-sm">Cliquez ici</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                                <img src="assets/images/doctor2.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Espace Docteur</h6>
+                                <a href="hms/doctor" target="_blank">
+                                    <button class="btn btn-success btn-sm">Cliquez ici</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-sm-4 blog-smk">
+                        <div class="blog-single">
+
+                                <img src="assets/images/admin2.jpg" alt="">
+
+                            <div class="blog-single-det">
+                                <h6>Espace Admin</h6>
+                    
+                                <a href="hms/admin" target="_blank">
+                                    <button class="btn btn-success btn-sm">Cliquez ici</button>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                    
+
+                    
+                    
+                </div>
+            </div>
+            
+        </div>
+    </section>  
+
+
+
+
+
+
+
+    <!-- ################# Our Departments Starts Here#######################--->
+
+
+    <section id="services" class="key-features department">
+        <div class="container">
+            <div class="inner-title">
+
+                <h2>Nos Services</h2>
+                <p>Découvrez nos services et soins</p>
+            </div>
+
+            <div class="row">
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-heartbeat"></i>
+                        <h5>Cardiologie</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-ribbon"></i>
+                        <h5>Orthopédie</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                       <i class="fab fa-monero"></i>
+                        <h5>Neurologie</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-capsules"></i>
+                        <h5>Pipeline pharmaceutique</h5>
+                    </div>
+                </div>
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="fas fa-prescription-bottle-alt"></i>
+                        <h5>Pharmacie</h5>
+                    </div>
+                </div>
+
+
+
+                <div class="col-lg-4 col-md-6">
+                    <div class="single-key">
+                        <i class="far fa-thumbs-up"></i>
+                        <h5>Soins de haute qualité</h5>
+
+                    </div>
+                </div>
+            </div>
+
+
+
+
+
+
+        </div>
+
+    </section>
+    
+    
+  
+    
+    <!--  ************************* About Us Starts Here ************************** -->
+        
+    <section id="about_us" class="about-us">
+        <div class="row no-margin">
+            <div class="col-sm-6 image-bg no-padding">
+                
+            </div>
+            <div class="col-sm-6 abut-yoiu">
+                <h3>À propos de Santé Connect</h3>
+<?php
+$ret=mysqli_query($con,"select * from tblpage where PageType='aboutus' ");
+while ($row=mysqli_fetch_array($ret)) {
+?>
+
+    <p><?php  echo $row['PageDescription'];?>.</p><?php } ?>
+            </div>
+        </div>
+    </section>    
+    
+    
+            <!--  ************************* Gallery Starts Here ************************** -->
+        <div id="gallery" class="gallery">    
+           <div class="container">
+              <div class="inner-title">
+
+                <h2>Notre Galerie</h2>
+                <p>Visiter la Galerie</p>
+            </div>
+              <div class="row">
+                
+
+        <div class="gallery-filter d-none d-sm-block">
+            <button class="btn btn-default filter-button" data-filter="all">Tout</button>
+            <button class="btn btn-default filter-button" data-filter="hdpe">Dentisterie</button>
+            <button class="btn btn-default filter-button" data-filter="sprinkle">Cardiologie</button>
+            <button class="btn btn-default filter-button" data-filter="spray"> Neurologie</button>
+            <button class="btn btn-default filter-button" data-filter="irrigation">Laboratoire</button>
+        </div>
+        <br/>
+
+
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="assets/images/gallery/dental.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter sprinkle">
+                <img src="assets/images/gallery/cardiology.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter hdpe">
+                <img src="assets/images/gallery/gallery_03.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter irrigation">
+                <img src="assets/images/gallery/kabir.jpg" class="img-responsive">
+            </div>
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+                <img src="assets/images/gallery/gallery_05.jpg" class="img-responsive">
+            </div>
+
+          
+
+            <div class="gallery_product col-lg-4 col-md-4 col-sm-4 col-xs-6 filter spray">
+                <img src="assets/images/gallery/gallery_06.jpg" class="img-responsive">
+            </div>
+
+        </div>
+    </div>
+       
+       
+       </div>
+        <!-- ######## Gallery End ####### -->
+    
+    
+     <!--  ************************* Contact Us Starts Here ************************** -->
+    
+    <section id="contact_us" class="contact-us-single">
+        <div class="row no-margin">
+
+            <div  class="col-sm-12 cop-ck">
+                <form method="post">
+                <h2 >Formulaire de contact</h2>
+                    <div class="row cf-ro">
+                        <div  class="col-sm-3"><label>Entrez votre Nom :</label></div>
+                        <div class="col-sm-8"><input type="text" placeholder="Entrez votre Nom" name="fullname" class="form-control input-sm" required ></div>
+                    </div>
+                    <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>E-mail :</label></div>
+                        <div class="col-sm-8"><input type="text" name="emailid" placeholder="Entrez votre Adresse E-mail" class="form-control input-sm"  required></div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>Téléphone :</label></div>
+                        <div class="col-sm-8"><input type="text" name="mobileno" placeholder="Entrez votre Numéro de téléphone" class="form-control input-sm" required ></div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label>Message:</label></div>
+                        <div class="col-sm-8">
+                          <textarea rows="5" placeholder="Entrez votre  Message" class="form-control input-sm" name="description" required></textarea>
+                        </div>
+                    </div>
+                     <div  class="row cf-ro">
+                        <div  class="col-sm-3"><label></label></div>
+                        <div class="col-sm-8">
+                         <button class="btn btn-success btn-sm" type="submit" name="submit">Envoyez votre Message</button>
+                        </div>
+                </div>
+            </form>
+            </div>
+     
+        </div>
+    </section>
+    
+    
+    
+    
+    
+    <!-- ################# Footer Starts Here#######################--->
+
+
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+       
+                <div class="col-md-6 col-sm-12">
+                    <h2>Liens utiles</h2>
+                    <ul class="list-unstyled link-list">
+                        <li><a ui-sref="about" href="#about_us">À propos de nous</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="portfolio" href="#services">Services</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="products" href="#logins">Connexions</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="gallery" href="#gallery">Galerie</a><i class="fa fa-angle-right"></i></li>
+                        <li><a ui-sref="contact" href="#contact_us">Contactez-nous</a><i class="fa fa-angle-right"></i></li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-sm-12 map-img">
+                    <h2>Contact</h2>
+                    <address class="md-margin-bottom-40">
+
+<?php
+$ret=mysqli_query($con,"select * from tblpage where PageType='contactus' ");
+while ($row=mysqli_fetch_array($ret)) {
+?>
+
+
+                        <?php  echo $row['PageDescription'];?> <br><br>
+                        Téléphone : <?php  echo $row['MobileNumber'];?> <br>
+                        Email : <a href="mailto:<?php  echo $row['Email'];?>" class=""><?php  echo $row['Email'];?></a><br>
+                        Horaires : <?php  echo $row['OpenningTime'];?>
+                    </address>
+
+        <?php } ?>
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+        
+
+    </footer>
+    <div class="copy">
+            <div class="container">
+        <center>Santé Connect</center> 
+                
+     
+            </div>
+
+        </div>
+    
+    </body>
+
+<script src="assets/js/jquery-3.2.1.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/jquery.easing.min.js"></script>
+<script src="assets/plugins/scroll-nav/js/scrolling-nav.js"></script>
+<script src="assets/plugins/scroll-fixed/jquery-scrolltofixed-min.js"></script>
+
+<script src="assets/js/script.js"></script>
+
+
+
+</html>
